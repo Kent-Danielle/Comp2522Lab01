5954a893a00637fde64c7d10e650cff357d517dd
Kent-Danielle <kconcengco@my.bcit.ca>
2022-09-06 15:15:00 -0700

    Fixed checkstyle errors

--- src/main/java/org/comp2522/Main.java
+++ src/main/java/org/comp2522/Main.java
@@ -1,7 +1,19 @@
 package org.comp2522;
 
+/**
+ * Print a simple message.
+ *
+ * @author Kent
+ * @version 1.0
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("I love bread");
-    }
+
+  /**
+   * Prints the message.
+   *
+   * @param args unused
+   */
+  public static void main(String[] args) {
+    System.out.println("I love bread");
+  }
 }
